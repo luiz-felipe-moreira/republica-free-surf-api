94e5ecaa70b50f27db9c514f294a35729f23775d
Luiz Felipe Moreira <felipecbm@gmail.com>
2017-08-12 23:59:21 -0300

    building creation of members

--- routes/membrosRouter.js
+++ routes/membrosRouter.js
@@ -1,7 +1,9 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 
 var Membros = require('../models/membros');
 var membroRouter = express.Router();
+membroRouter.use(bodyParser.json());
 
 membroRouter.route('/').
   get(function (req, res, next) {
@@ -9,6 +11,20 @@ membroRouter.route('/').
       if (err) next(err);
       res.json(membros);
     })
-  });
+  })
+  
+  .post(function (req, res, next) {
+    Membros.create(req.body, function (err, membro) {
+        if (err) next(err);
+        console.log('Membro criado!');
+        res.json(membro);
+        // var nome = membro.nome;
+        // res.writeHead(200, {
+        //     'Content-Type': 'text/plain'
+        // });
+
+        // res.end('Adicionado membro: ' + nome);
+    });
+});
 
 module.exports = membroRouter;
