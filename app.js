62084148f68c7a588193c5ba7d3c458aaa018eb5
Luiz Felipe Moreira <felipecbm@gmail.com>
2017-08-27 14:59:49 -0300

    Habilitando CORS

--- app.js
+++ app.js
@@ -33,7 +33,12 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'dist')));
+// app.use(express.static(path.join(__dirname, 'dist')));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.use('/', index);
 app.use('/users', users);
