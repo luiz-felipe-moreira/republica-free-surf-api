db74f947d8618c6e96801376360edab0f372e6fa
Luiz Felipe Moreira <felipecbm@gmail.com>
2018-04-30 12:44:43 -0300

    undo mistaken commit

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    //'mongoUrl' : 'mongodb://heroku_qd9p98fv:p5qmguqsho9366q5t1gp9somhe@ds149613.mlab.com:49613/heroku_qd9p98fv',
-    'mongoUrl' : 'mongodb://localhost:27017/republicaFreeSurf',
+    'mongoUrl' : 'mongodb://heroku_qd9p98fv:p5qmguqsho9366q5t1gp9somhe@ds149613.mlab.com:49613/heroku_qd9p98fv',
+    //'mongoUrl' : 'mongodb://localhost:27017/republicaFreeSurf',
     's3Bucket' : 'republica-free-surf-photos'
 }
