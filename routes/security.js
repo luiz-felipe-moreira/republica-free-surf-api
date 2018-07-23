e31c02f0842d04e109f0e94126e299758a1005f9
Luiz Felipe Moreira <felipecbm@gmail.com>
2018-07-22 22:01:07 -0300

    bug fix

--- routes/security.js
+++ routes/security.js
@@ -16,12 +16,11 @@ exports.authenticate = expressJwt({
 
 exports.verifyAdmin = function (req, res, next) {
 
-    if (process.env.NODE_ENV === 'development') {
-        next();
-    } else if ((req.auth === undefined) || (!req.auth.admin)) {
+    if (authenticationRequired && ((req.auth === undefined) || (!req.auth.admin))) {
         var err = new Error('You are not authorized to access this resource');
         err.status = 403;
         return next(err);
+    } else {
+        next();
     }
-    next();
 };
